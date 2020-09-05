d8db9515845ad64042b4868e145154807ba55abf
Emanuel Felipe <e.felipe_efmb@hotmail.com>
2020-09-05 06:10:15 -0400

    Update index.js

--- js/rock_level_1/Works/aulajs/index.js
+++ js/rock_level_1/Works/aulajs/index.js
@@ -1,2 +1,10 @@
-const nome = "EF_ohh!"; // In "js" you may or may not use ";" to end an instruction.
-console.log(nome)       // node installed, 
+/*const nome = "EF_ohh!"; // In "js" you may or may not use ";" to end an instruction.
+console.log(name)       //  Check installations on the Ctrl+J console, node index.js, node -v, np, -v, choco -v*/
+
+// Create a program that averages grades between students and sends an avarege score message.
+
+const name = "Mayk"
+const name = 'Diego'
+const name = `Valeska e ${name}`// Template string (You can insert a variable by calling another variable).
+
+console.log(name3)
