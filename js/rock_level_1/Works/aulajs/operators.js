43306c432188070563ef48df7100b3625043cd59
Emanuel Felipe <e.felipe_efmb@hotmail.com>
2020-09-10 00:38:57 -0400

    Update operators.js

--- js/rock_level_1/Works/aulajs/operators.js
+++ js/rock_level_1/Works/aulajs/operators.js
@@ -27,7 +27,7 @@ console.log(4 !== "5") // true
 =============================================================================*/
 
 // Challenger 01
-// Check if the personis over 18 years old. >>> 
+// Check if the personis over 18 years old. >>> Make sure the person's age is 18 uear or order.
 // If so, let it in.
 // Otherwise, block the entry.
 // If the person is 17 years old, tell him to come back when turns 18.
