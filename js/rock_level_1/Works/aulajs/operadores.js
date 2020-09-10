b79a82614e3d68501702105f2f9f548731def29d
Emanuel Felipe <e.felipe_efmb@hotmail.com>
2020-09-10 00:08:26 -0400

    Create operadores.js

---
+++ js/rock_level_1/Works/aulajs/operadores.js
@@ -0,0 +1,11 @@
+/*============================================================================
+    COPARISON OPERATORS
+
+    >      Bigger.
+    <      Smaller.
+    >=     Bigger than.
+    <=     Less than.
+    ==     Equal.
+    ===    Same and the same type.
+    !=     Different than.
+    !===   Different, even of the same type.
