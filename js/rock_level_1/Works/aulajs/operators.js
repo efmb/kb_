43ab207847b87bcaa744b06799e3aa96de2d6aff
Emanuel Felipe <e.felipe_efmb@hotmail.com>
2020-09-10 00:56:19 -0400

    Update operators.js

--- js/rock_level_1/Works/aulajs/operators.js
+++ js/rock_level_1/Works/aulajs/operators.js
@@ -32,9 +32,13 @@ console.log(4 !== "5") // true
 // Otherwise, block the entry.
 // If the person is 17 years old, tell him to come back when turns 18.
 
-const age = 17,99
+const age = 17 // If it contains a comma ",", please review the error.
 if(age >= 18) {
-    console.log("Deixar entrar")
+    console.log("Let it in.")
 } else {
-    console.log("Bloquear a entrada")
+    console.log("Block the entrance or Entry blocked.")
+}
+
+if(age === 17){
+    console.log("Come back when you're 18")
 }
