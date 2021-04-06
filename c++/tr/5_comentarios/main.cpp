f784ef24fc2954c55e2edf41b47e7c6f9d971d01
Emanuel Felipe <e.felipe_efmb@hotmail.com>
2021-04-05 20:34:14 -0400

    update
    Signed-off-by: Emanuel Felipe <e.felipe_efmb@hotmail.com>

---
+++ c++/tr/5_comentarios/main.cpp
@@ -0,0 +1,10 @@
+// meu primeiro programa
+#include <iostream> // esse é o cabeçalho
+using namespace std; // isso aqui serve para usar as instruções cout e [endl]
+int main(){ // essa função é fundamental
+    cout << "Meu primeiro programa em C++" << '\n'; // isso será impresso na tela
+        //cout << "Meu primeiro programa em C++" << '\n'; // usando o namesapce
+        //std::cout << "Meu primeiro programa em C++" << std::endl; // sem usar namespace
+        return 0; // fundamental para sair do programa, tem de existir em toda função main
+
+}
--- c++/tr/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(){
-    std::cout << "Meu primeiro programa em C++" << std::endl;
-    return 0;
-}
